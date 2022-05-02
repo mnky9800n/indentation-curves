d29ac318abb5bfdd1db452a11b3f23a45049a78e
mnky9800n <johnm.aiken@gmail.com>
2022-05-02 15:33:53 +0200

    made polyfit

--- explorations.r
+++ explorations.r
@@ -1,20 +1,37 @@
 library(readxl)
 # library(lme4)
+# library(ggplot2)
 
 green <- read_excel(filename<-"green_010_hfsm3_50#1.xlsm", sheet<-"curves processed")
 strain <- green$`A: Strain`
 stress <- green$`A: Stress`
-df <- data.frame(stress<-stress, strain<-strain)
+df <- data.frame(stress=stress, strain=strain)
 plot(strain, stress)
+# ggplot(strain, stress)
 
+# linear naive model
+# the line whre its like
+# $ data.frame(strain<-x)
+# is because the model A) requires a data.frame object
+# and B) because it requires that data.frame to have a
+# column called "strain"
 model <- lm(stress~strain, data=df)
-summary(model)
-
 x <- seq(from=0, to=0.4, by=0.001)
 y <- predict(object<-model, newdata<-data.frame(strain<-x))
-lines(x, y, col='red')
-length(x)
-length(y)
-y
-length(model)
-summary(model)
+lines(x, y, col='red', lty=1)
+
+# linear model, <0.1 strain
+df_leq01 <- df[df$strain < 0.1,]
+model <- lm(stress~strain, data=df_leq01)
+y <- predict(object<-model, newdata<-data.frame(strain<-x))
+lines(x, y, col='aquamarine', lty=1, lwd=2)
+
+# polynomial naive model
+# df_leq01 <- df[df$strain < 0.1,]
+model <- lm(stress~poly(strain, 3), data=df)
+y <- predict(object<-model, newdata<-data.frame(strain<-x))
+lines(x, y, col='gold', lty=1, lwd=2)
+
+
+legend(x<-0.02, y<-50, legend<-c('naive, linear', 'strain<0.1, linear', 'polynomial'), lty=1, col=c('red', 'aquamarine', 'gold'))
+
